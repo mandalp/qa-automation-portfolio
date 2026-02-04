36ab910192568125f9da3fd14a6f34d06d84f958
Amanda <amandalops00@gmail.com>
2026-02-04 11:57:14 -0300

    configuring githubActions

--- api/cypress.config.js
+++ api/cypress.config.js
@@ -2,11 +2,12 @@ const { defineConfig } = require("cypress");
 const { deleteUserByEmail } = require("./cypress/support/database");
 
 module.exports = defineConfig({
+  projectId: "zaf1sh",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
-        deleteUser(email){
+        deleteUser(email) {
           return deleteUserByEmail(email)
         }
       })
